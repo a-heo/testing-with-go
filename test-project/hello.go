35d1609f77585cd0a5ad9c730b5b3e30790d4c08
annie heo <annie.heo@radarfirst.com>
2022-03-25 12:44:00 -0700

    write tests for greetings in different languages

--- test-project/hello.go
+++ test-project/hello.go
@@ -3,14 +3,31 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const koreanHelloPrefix = "Ahnyoung, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name =="" {
 		name = "world"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
 }
 
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "french":
+		prefix = frenchHelloPrefix
+	case "spanish":
+		prefix = spanishHelloPrefix
+	case "korean":
+		prefix = koreanHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
