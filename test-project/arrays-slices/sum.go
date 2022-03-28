065adadfa252fa838ad8d45ff8cdcc78423fb56a
annie heo <annie.heo@radarfirst.com>
2022-03-28 11:40:10 -0700

    implement tests and functions for arrays and slices

--- test-project/arrays-slices/sum.go
+++ test-project/arrays-slices/sum.go
@@ -1,9 +1,35 @@
-package main 
+package main
 
-func Sum(numbers []int) int{
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbers ...[]int) []int {
+	// lenNums := len(numbers)
+	totalsums := make([]int, len(numbers))
+
+	for i, nums := range numbers {
+		totalsums[i] = Sum(nums)
+	}
+
+	return totalsums
+}
+
+func SumAllTails(numbers ...[]int) []int {
+	var totalTails []int 
+
+	for _, nums := range numbers {
+		if len(nums) == 0 {
+			totalTails = append(totalTails, 0)
+		} else {
+			tail := nums[1:]
+			totalTails = append(totalTails, Sum(tail))
+		}
+	}
+
+	return totalTails
 }
