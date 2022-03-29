6713263197a9548a06890244b4e14a4176a3a594
annie heo <annie.heo@radarfirst.com>
2022-03-29 16:05:41 -0700

    rewrite parts of tests for readability

--- test-project/structs_methods_interfaces/shapes.go
+++ test-project/structs_methods_interfaces/shapes.go
@@ -2,9 +2,17 @@ package main
 
 import "math"
 
+type Shape interface{
+	Area() float64
+}
+
 type Rectangle struct {
 	Width float64
 	Height float64
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
 }
 
 func (r Rectangle) Area() float64 {
@@ -19,6 +27,11 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+type Triangle struct {
+	Base float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
 }
--- test-project/structs_methods_interfaces/shapes_test.go
+++ test-project/structs_methods_interfaces/shapes_test.go
@@ -2,37 +2,37 @@ package main
 
 import "testing"
 
-// func TestPerimeter(t *testing.T) {
-// 	rectangle := Rectangle{10.0, 10.0}
-// 	got := rectangle.Perimeter()
-// 	want := 40.0
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := rectangle.Perimeter()
+	want := 40.0
 
-// 	if got != want {
-// 		t.Errorf("got %.2f want %.2f", got, want)
-// 	}
-// }
-
-//makes helper decoupled from concrete types
-type Shape interface {
-	Area() float64
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
 }
+
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
+	//declaring a slice of struct using anon. w/ 2 fields and fill it with cases
+	areaTests := []struct {
+		shape Shape
+		hasArea float64
+	}{
+		//easier to understand case fields  
+		{shape: Rectangle{12, 6}, hasArea: 72.0},
+		{shape: Circle{10}, hasArea: 314.1592653589793},
+		{shape: Triangle{12, 6}, hasArea: 36.0},
+
+		//this is still acceptable
+		// {Rectangle{12, 6}, 72.0},
+		// {Circle{10}, 314.1592653589793},
+		// {Triangle{12, 6}, 36.0},
+	}
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.hasArea {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
 		}
 	}
-
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		checkArea(t, rectangle, 100.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{5.0}
-		checkArea(t, circle, 78.53981633974483)
-	})
 }
