79aaacb04861d10155116fdd25bfc09d94cab18a
annie heo <annie.heo@radarfirst.com>
2022-04-01 13:17:27 -0700

    create new func and test funcs for dictionary (maps session)

---
+++ test-project/maps/dictionary_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		// got := Search(dictionary, "test")
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		// err := Search(dictionary, "unknown")
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you're looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
+
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
