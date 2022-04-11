8c61a9bad4d57aa68d9e5f7f018045efd4fb1528
annie heo <annie.heo@radarfirst.com>
2022-04-11 13:51:19 -0700

    refactor code with a reusable function to use in both cli and webserver main.go

--- test-project/commandline/cmd/webserver/main.go
+++ test-project/commandline/cmd/webserver/main.go
@@ -1,33 +1,26 @@
 package main
 
 import (
-	"github.com/a-heo/testing-with-go/tree/main/test-project/commandline"
+	poker "github.com/a-heo/testing-with-go/tree/main/test-project/commandline"
 	"log"
 	"net/http"
-	"os"
 )
 
 //file for db
 const dbFileName = "../../game.db.json"
 
 func main() {
-	//create file for db, second arg defines permission (read and write or create if it doesn't exist), 3rd arg sets permission for files (all users can read and write) 
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
 	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
+		log.Fatal(err)
 	}
+	defer close()
 
-	store, err := poker.NewFileSystemPlayerStore(db)
-
-	if err != nil {
-		log.Fatalf("could not create file system player store, %v", err)
-	}
-	
 	server := poker.NewPlayerServer(store)
 
-	if err := http.ListenAndServe(":5002", server); err != nil {
-		log.Fatalf("could not listen on port 5000 %v", err)		
+	if err := http.ListenAndServe(":5003", server); err != nil {
+		log.Fatalf("could not listen on port 5003 %v", err)		
 	}
 
 }
